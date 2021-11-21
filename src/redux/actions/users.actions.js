90964cab11060d818f613f93371bcccbe3fb4d4e
george <heorhii.makeiev@gmail.com>
2021-11-21 19:34:06 +0300

    front_react

--- src/redux/actions/users.actions.js
+++ src/redux/actions/users.actions.js
@@ -5,19 +5,19 @@ const INPUT_ERROR = 'input_error';
 
 const stateFormUser = (payload) => {
     return {type: FORM_USER, payload};
-}
+};
 
 const stateFormMessage = (payload) => {
     return {type: FORM_MESSAGE, payload};
-}
+};
 
 const stateUser = (payload) => {
     return {type: USER, payload};
-}
+};
 
 const stateInputError = (payload) => {
     return {type: INPUT_ERROR, payload};
-}
+};
 
 export {
     FORM_USER, stateFormUser,
